f331101b18128605dab19ba5b92f0eb9a923e1ae
Toni Vanhala <toni.vanhala@metosin.fi>
2020-02-20 19:15:03 +0200

    Add small server example

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "Clojure(Script) training"
   :url "https://github.com/metosin/training-day-1"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [http-kit "2.3.0"]
+                 [metosin/jsonista "0.2.5"]]
   :plugins [[nightlight/lein-nightlight "RELEASE"]]
   :source-paths ["src"]
   :target-path "target/%s/"
--- src/main.clj
+++ src/main.clj
@@ -1,8 +1,34 @@
-(ns main)
+(ns main
+  (:require [org.httpkit.server :refer [run-server]]
+            [jsonista.core :as json]))
 
-(defn hello
-  [name]
-  (println "Hello, " name))
+(defn parse-params
+  [query-string]
+  (->> (re-seq #"([^=^&]+)=(\d+)" query-string)
+       (reduce (fn [acc [_ k v]] (assoc acc ( keyword k) v)) {})))
 
-(comment
-  (hello "Morty Smith"))
+(defn handle
+  [req]
+  (case (:uri req)
+    "/"
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body (json/write-value-as-string {"statement" "Clojure is quite good!"})}
+    "/add"
+    (let [params (some-> req :query-string (parse-params))
+          a (some-> (:a params) (Integer/parseInt))
+          b (some-> (:b params) (Integer/parseInt))]
+      (if (and a b)
+        {:status 200
+         :headers {"Content-Type" "application/json"}
+         :body (json/write-value-as-string {"a" a
+                                            "b" b
+                                            "sum" (+ a b)})}
+        {:status 422}))
+    {:status 404}))
+
+(defn -main
+  [& args]
+  (let [port (-> (System/getenv "PORT") (Integer/parseInt))]
+    (run-server handle {:port port})
+    (println "Server running in port" port)))
